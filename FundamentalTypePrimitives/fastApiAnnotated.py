84f65c59e051fae8fbe6920ff1ac48bf8f6bb784
JhonathanHerrera <herrerajhonathan@ufl.edu>
2025-12-28 00:15:45 -0500

    finished example

--- FundamentalTypePrimitives/fastApiAnnotated.py
+++ FundamentalTypePrimitives/fastApiAnnotated.py
@@ -1,3 +1,16 @@
-from fastapi import FastAPI
+# uvicorn fastApiAnnotated:app --reload
+
+from typing import Annotated
+from fastapi import FastAPI, Query
 
 app = FastAPI()
+
+@app.get("/items/")
+def readCharlieBrownCharacters(
+    name: Annotated[str | None, Query(min_length=3, max_length=50)] = None,
+    age: Annotated[int, Query(gt=0, le=100)] = 10,
+):
+    return {
+        "name": name,
+        "age": age,
+    }
