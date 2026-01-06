d3fe17515d5e15333b3173888fdbbb613fff217d
JhonathanHerrera <herrerajhonathan@ufl.edu>
2026-01-05 21:33:57 -0500

    Venmo with Union

--- FundamentalTypePrimitives/union.py
+++ FundamentalTypePrimitives/union.py
@@ -1,1 +1,34 @@
-from typing import Union
+from typing import Union, Literal
+
+class Venmo:
+    def __init__(self, amount: Union[int, float]):
+        self.total_amount = amount
+    
+    def action(
+        self,
+        amount: Union[int, float],
+        action: Literal["Withdraw", "Deposit"],
+    ) -> None:
+        if isinstance(amount, bool):
+            raise TypeError("amount must be int or float, not bool")
+    
+        if action == "Withdraw":
+            if amount <= self.total_amount:
+                self.total_amount -= amount
+            else:
+                raise ValueError("Insufficient funds")
+        
+        elif action == "Deposit":
+            self.total_amount += amount
+        
+    def print_total_amount(self) -> None:
+        print(self.total_amount)
+
+
+Jhons_Venmo = Venmo(24.5)
+Jhons_Venmo.print_total_amount()
+Jhons_Venmo.action(30, "Deposit")
+Jhons_Venmo.action(400.23, "Deposit")
+Jhons_Venmo.action(20.22, "Withdraw")
+Jhons_Venmo.print_total_amount() # 434.51
+
