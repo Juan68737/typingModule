e5708086ad874ca430a35621ca4f4595def218fd
JhonathanHerrera <herrerajhonathan@ufl.edu>
2026-01-11 22:15:08 -0500

    concatenate

--- FundamentalTypePrimitives/concatenate.py
+++ FundamentalTypePrimitives/concatenate.py
@@ -1,3 +1,21 @@
-from typing import Concatenate
+from typing import Concatenate, ParamSpec, Callable, TypeVar, List
 
-#idk
+P = ParamSpec("P")
+R = TypeVar("R")
+
+def validate_int_list(func: Callable[P, R]) -> Callable[Concatenate[str, P], R]:
+    def wrapper(name: str, *args: P.args, **kwargs: P.kwargs) -> R:
+        print(f"{name} is calling this function")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@validate_int_list
+def sum_of_list(values: List[int]) -> int:
+    return sum(values)
+
+'''
+Bruno is calling this function
+15
+'''
+print(sum_of_list("Bruno",[1, 2, 3, 4, 5]))    
